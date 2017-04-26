8d87433d5f98c4a9f1a7ab35b192db53a804844a
ikroorda <ingrid.roorda@gmail.com>
2017-04-27 00:59:28 +0200

    Update mapit.py

--- mapit.py
+++ mapit.py
@@ -6,18 +6,20 @@ import folium
 ##Get a list of all the locations (lat, long) and put points on osm map
 def Map(locations, colors):
         colors_n = len(colors)
-        loc_n = len(locations)
-        mod = int(loc_n/colors_n) + 1
-        print mod
-	map_osm = folium.Map(location=[52.4919132, 13.3955423], tiles = 'Stamen Toner', zoom_start=12)
+	center = [0,0]
+	for item in locations:
+		center[0] += item[0]
+		center[1] += item[1]
+	center[0] = center[0]/len(locations)
+	center[1] = center[1]/len(locations)
+	map_osm = folium.Map(location=[center[0], center[1]], tiles = 'Stamen Toner', zoom_start=12)
 
 #To print dots with color gradient instead of lines. Very slow.
+#        loc_n = len(locations)
+#       mod = int(loc_n/colors_n) + 1
 #	for index, item in enumerate(locations):
 #		folium.CircleMarker(location=[item[0],item[1]], radius=4,  popup='', color= colors[int(index/mod)],fill_color=colors[int(index/mod)]).add_to(map_osm)
 
-
-
-         
 #To print a line
         folium.PolyLine(locations, color="red", weight=2.5, opacity=1).add_to(map_osm)
 
